0f40095072ab068d6dc96813ce71f43e501d6554
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-22 22:33:59 +0530

    learnt cjs and mjs

---
+++ 02-modularization/mjs/additionModule.js
@@ -0,0 +1,6 @@
+export function calculateSum(x, y) {
+   const result = x + y;
+   console.log(result);
+}
+
+export const author = 'Harsh Tambe';
---
+++ 02-modularization/mjs/app.js
@@ -0,0 +1,6 @@
+import { calculateSum, author } from './additionModule.js';
+
+calculateSum(15, 24);
+console.log('Author:', author);
+
+console.log('This is app.js file');
