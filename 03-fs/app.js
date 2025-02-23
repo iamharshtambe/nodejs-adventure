2cc30a7e7130f62fe3727fe01a9f40b729f657d9
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-23 17:20:36 +0530

    learnt fs in Node.js

--- 03-fs/app.js
+++ 03-fs/app.js
@@ -1,10 +1,54 @@
 const fs = require('fs');
 
 // reading files
-fs.readFile('./data/myInfo.txt', 'utf-8', (err, data) => {
+fs.readFile('./data/myInfo1.txt', 'utf-8', (err, data) => {
    if (err) {
       console.log(err);
    } else {
       console.log(data);
    }
 });
+
+// writing files
+fs.writeFile('./data/myInfo2.txt', 'Harsh Vijay Tambe', 'utf-8', (err) => {
+   if (err) {
+      console.log(err);
+   } else {
+      console.log('File is written successfully');
+   }
+});
+
+// creating files
+fs.writeFile(
+   './data/myInfo3.txt',
+   'JavaScript is Awesome!!!',
+   'utf-8',
+   (err) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log('File is created successfully');
+      }
+   }
+);
+
+// creating and deleting directory (if already exists)
+if (fs.existsSync('./testDir')) {
+   fs.rmdir('./testDir', (err) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log('Folder is deleted successfuly');
+      }
+   });
+} else {
+   fs.mkdir('./testDir', (err) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log('Folder is created successfully');
+      }
+   });
+}
+
+// for deleting files use unlink()
