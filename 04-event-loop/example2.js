e904e97dccb614d73ad23f9d77ed09a6f2361b87
iamharshtambe <harshtambe.dev@gmail.com>
2025-02-25 23:07:14 +0530

    converted into examples

---
+++ 04-event-loop/example2.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const a = 100;
+
+setImmediate(() => console.log('setImmediate'));
+
+Promise.resolve(() => console.log('Promise'));
+
+fs.readFile('./test.txt', 'utf8', () => {
+   console.log('File reading CB');
+});
+
+setTimeout(() => {
+   console.log('Timer expired');
+}, 0);
+
+process.nextTick(() => console.log('process.nextTick()'));
+
+function printA() {
+   console.log('a =', a);
+}
+
+printA();
+
+console.log('Last line of the file');
+
+//100
+// last
+// process
+// promise
+// Timer
+// setImm
+// fs
